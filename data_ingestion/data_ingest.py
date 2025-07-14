89e1ed6fbd8594bf23fa1f106534d8c2946d02c9
Deepu122 <deepthig122@gmail.com>
2025-07-15 02:39:56 +0530

    class skeleton created

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -3,3 +3,11 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from data_ingestion.data_transform import data_converter
+
+
+class ingest_data:
+    def __init__(self):
+        pass
+
+    def data_ingestion(self):
+        pass
