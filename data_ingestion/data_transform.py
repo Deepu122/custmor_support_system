7c80e062bd251fe5f9942db0e167ed7de8db26af
Deepu122 <deepthig122@gmail.com>
2025-07-15 03:42:06 +0530

    in data csv file updated

--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -5,9 +5,16 @@ from langchain_core.documents import Document
 
 class data_converter:
     def __init__(self):
-        pass
+        print("data convert has been init...")
 
     def data_transformation(self):
         pass
+    
+        
+
+if __name__ == '__main__':
+    data_con=data_converter()
+
+
         
 
